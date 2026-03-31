1934f5607c0edde47670a01858e33b2fad8a9f52
manikandanemed <manikandan@emedlogix.com>
2026-03-31 15:56:53 +0530

    fixcycleerror

--- HospitalProject/Program.cs
+++ HospitalProject/Program.cs
@@ -77,7 +77,14 @@ builder.Services.AddAuthorization();
 // =========================
 // CONTROLLERS + SWAGGER
 // =========================
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler =
+            System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
--- HospitalProject/obj/Debug/net8.0/HospitalProject.AssemblyInfo.cs
+++ HospitalProject/obj/Debug/net8.0/HospitalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0415b6ab589eceeb240b42b417e1deeac4e262bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b08995090e0b55f0b9b20716b904cf09ba7c959d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
